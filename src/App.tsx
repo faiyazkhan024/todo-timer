32b005dcddcb8234b3cf1ef2bc5978cf72079705
Faiyaz Khan <faiyazkhan024@gmail.com>
2025-03-20 22:34:34 +0530

    Uodated App & index

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
-const App = () => {
-  return <h1>Todo Timer</h1>;
+import { FC } from "react";
+
+import { TodoProvider } from "./providers/TodoProvider";
+import { AddTodo } from "./components/AddTodo";
+import { TodoList } from "./components/TodoList";
+
+const App: FC = () => {
+  return (
+    <TodoProvider>
+      <AddTodo />
+      <TodoList />
+    </TodoProvider>
+  );
 };
 
 export default App;
